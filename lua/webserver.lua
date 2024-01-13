dc51b166c71fe6e0c2cf05d4ce899d6892cdcab9
Guy John <guy@rumblesan.com>
2024-01-13 20:54:59 +0000

    Adding healthcheck endpoint to api server

--- lua/webserver.lua
+++ lua/webserver.lua
@@ -1,46 +1,24 @@
 local app = require("milua")
 
--- Basic example
-app.add_handler(
-    "GET",
-    "/",
+app.add_handler("GET", "/",
     function()
-        return "<h1>Welcome to the <i>handsome</i> server!</h1>", {
+        return "<h1>Memento Radio!</h1>", {
             ["Content-Type"] = "text/html"
         }
     end
 )
 
--- Example capturing a path variable
-app.add_handler(
-    "GET",
-    "/user/...",
-    function (captures, query, headers)
-        local username = captures[1]
-        local times = query.times or 1
-        return "The user " .. username .. " is" .. (" very"):rep(times) .. " handsome"
+app.add_handler("GET", "/health",
+    function()
+        return "OK", {
+            ["Content-Type"] = "text/plain"
+        }
     end
 )
-
--- Example returning no data and status
-app.add_handler(
-    "DELETE",
-    "/user",
-    function ()
-        return nil, { [":status"] = "204" }
-    end
-)
-
--- Hooking the server close event
---app.shutdown_hook(function()
-    -- cleaning up any external resource
---end)
-
-local app_config = {
-    HOST = API_HOST,
-    PORT = API_PORT
-}
 
 print(" INFO: [API Server] running at " .. API_HOST .. ":" .. API_PORT)
 
-app.start(app_config)
+app.start({
+    HOST = API_HOST,
+    PORT = API_PORT
+})
